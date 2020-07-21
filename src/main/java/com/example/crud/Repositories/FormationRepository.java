539ee0887171333f5829685bac8862b5b211980f
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-07-21 13:28:37 +0100

    Update FormationRepository.java

--- src/main/java/com/example/crud/Repositories/FormationRepository.java
+++ src/main/java/com/example/crud/Repositories/FormationRepository.java
@@ -2,8 +2,10 @@ package com.example.crud.Repositories;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import com.example.crud.Entities.Formation;
 
 public interface FormationRepository extends CrudRepository<Formation, Long> {
-
+List<Formation> findByTitre(String titre);
+List<Formation> findByTitreAndFormateur(String titre,String formateur);
 }
